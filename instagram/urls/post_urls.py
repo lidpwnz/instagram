6fdbb2e7b997dc5f81427e560b1b0d7e851a7ef2
davidlee <lidpwnz@gmail.com>
2021-09-26 00:55:05 +0600

    #6 Реализовал добавление лайков для поста

--- instagram/urls/post_urls.py
+++ instagram/urls/post_urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from instagram.views.post_views import PostCreate
+from instagram.views.post_views import PostCreate, AddLikeToPostView
 
 urlpatterns = [
     path('create/', PostCreate.as_view(), name='post_create'),
+    path('<int:post_pk>/add_like', AddLikeToPostView.as_view(), name='add_like')
 ]
--- instagram/views/post_views.py
+++ instagram/views/post_views.py
@@ -1,8 +1,9 @@
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views import generic
 
 from instagram.forms.post_form import PostForm
-from instagram.models import Post
+from instagram.models import Post, Like
 
 
 class PostCreate(generic.CreateView):
@@ -15,3 +16,25 @@ class PostCreate(generic.CreateView):
 
     def get_success_url(self):
         return reverse_lazy('profile', kwargs={'pk': self.object.pk})
+
+
+class AddLikeToPostView(generic.View):
+    def get_post(self):
+        return Post.objects.get(pk=self.kwargs.get('post_pk'))
+
+    def post(self, request, *args, **kwargs):
+        user_who_likes = request.user
+        post = self.get_post()
+        if user_who_likes in post.users_who_like_it.all():
+            post.users_who_like_it.get(liked_user=user_who_likes).delete()
+            post.likes_count -= 1
+        else:
+            post.users_who_like_it.add(user_who_likes)
+            post.likes_count += 1
+
+        post.save()
+        return redirect(self.request.get_full_path)
+
+
+class AddCommentToPostView(generic.View):
+    pass
